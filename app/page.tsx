6c22a8f4b427f5535e19230d687c46246c90995b
mtalala <superraioi2014@gmail.com>
2026-02-27 13:40:12 -0300

    feat: create first UI version

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,60 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { FolderArchive } from "lucide-react";
 
 export default function Home() {
+  const [statusText, setStatusText] = useState("_");
+
+  const mission = {
+    message: "Olá, Terra! Recebemos sua mensagem e estamos prontos para conhecê-los!",
+  };
+
+  const handleContact = () => {
+    setStatusText("Enviando mensagem...");
+
+    setTimeout(() => {
+      setStatusText("Recebendo resposta...");
+
+      setTimeout(() => {
+        setStatusText(`Mensagem recebida: ${mission.message}`);
+      }, 1200);
+
+    }, 1200);
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div
+      className="flex flex-col items-center h-screen w-screen bg-cover bg-center p-10"
+      style={{ backgroundImage: "url('/unsplashspace.jpg')" }}
+    >
+      <h1 className="text-white text-5xl font-bold">Missão Espacial</h1>
+
+      <div className="w-[800px] bg-[#262629] rounded-[24px] mt-10 px-8 py-8 space-y-4">
+        <p className="text-white text-sm font-semibold">Upload de Arquivo</p>
+
+        <div className="flex items-center gap-2 bg-[#414141] rounded-[7px] w-fit py-[10px] px-[15px]">
+          <div className="bg-[#787878] px-[10px] py-[10px] rounded-[6px]">
+            <FolderArchive className="text-white" />
+          </div>
+          <p className="text-white text-sm font-semibold">
+            MensagemDaTerra.jpg
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        <button
+          onClick={handleContact}
+          className="text-white text-sm font-semibold py-2 px-3 bg-green-700 rounded-[8px]"
+        >
+          Fazer contato
+        </button>
+      </div>
+
+      <div className="w-[800px] h-[300px] bg-[#2f2e33] mt-10 border border-[#8f8f8f] px-[10px] py-[10px]">
+        <p className="text-white text-sm font-semibold">
+          {statusText}
+        </p>
+      </div>
     </div>
   );
 }
