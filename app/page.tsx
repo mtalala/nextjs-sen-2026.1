f3ffcc20a75670e11e88a91ce31d38d67ec3e034
mtalala <superraioi2014@gmail.com>
2026-03-16 23:08:15 -0300

    style: update background color

--- app/page.tsx
+++ app/page.tsx
@@ -63,8 +63,8 @@ export default function Home() {
           Upload de Arquivo
         </p>
 
-        <div className="flex items-center gap-2 bg-[#414141] rounded-[7px] w-fit py-[10px] px-[15px]">
-          <div className="bg-[#787878] px-[10px] py-[10px] rounded-[6px]">
+        <div className="flex items-center gap-2 bg-[#45454D] rounded-[7px] w-fit py-[10px] px-[15px]">
+          <div className="bg-[#696970] px-[10px] py-[10px] rounded-[6px]">
             <Folder className="text-white" />
           </div>
 
